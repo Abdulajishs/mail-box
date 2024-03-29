016c815a9f58b9f3f64605745b286ae980c781c9
Abdulajishs <abdulajeeshsb@gmail.com>
2024-03-29 10:59:25 +0530

    Signup Screen

---
+++ src/component/Auth/SignUpForm.js
@@ -0,0 +1,73 @@
+import { Button, Card, CardBody, FloatingLabel, Form } from "react-bootstrap";
+import classes from "./SignUpForm.module.css"
+import { useRef } from "react";
+import axios from "axios";
+
+const SignUpForm = () => {
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const confirmPasswordRef = useRef("");
+
+    const addSignupUser = async (email, password) => {
+        console.log(email, password);
+        try {
+            const response = await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBCP8zyM-IOpUl7LeZYlMyOrJLnjW1tQq8",
+                {
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+
+            console.log(response.data);
+            if (response.status === 200) {
+                console.log("User has successfully signed up");
+            } else {
+                throw new Error("Authentication is failed")
+            }
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = confirmPasswordRef.current.value;
+
+        if (password !== confirmPassword) {
+            alert("password doesn't match")
+            return
+        }
+        addSignupUser(email, password)
+
+        emailRef.current.value = ""
+        passwordRef.current.value = ""
+        confirmPasswordRef.current.value = ""
+    }
+
+    return (
+        <div className={`${classes.backgroundImage} d-flex flex-column justify-content-center align-items-center `}>
+            <Card className={`mb-3 ${classes.card}`}>
+                <CardBody className="border-1 p-3 d-flex flex-column align-items-center justify-content-around" >
+                    <h2 className="mb-4">SignUp</h2>
+                    <Form onSubmit={submitHandler}>
+                        <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+                            <Form.Control type="email" placeholder="name@example.com" ref={emailRef} required />
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+                            <Form.Control type="password" placeholder="Password" ref={passwordRef} required />
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingConfirmPassword" label="Confirm Password" className="mb-4">
+                            <Form.Control type="password" placeholder="Confirm Password" ref={confirmPasswordRef} required />
+                        </FloatingLabel>
+                        <Button variant="primary" className="w-100 border rounded-5 mb-3" type="submit" >SignUp</Button>
+                    </Form>
+                </CardBody>
+            </Card>
+            <Button variant="outline-success" className={classes.button}>Have an account?Login</Button>
+        </div>
+    )
+}
+
+export default SignUpForm
---
+++ src/page/SignUp.js
@@ -0,0 +1,10 @@
+import SignUpForm from "../component/Auth/SignUpForm";
+
+const SignUp = () =>{
+    return(
+    <>
+     <SignUpForm />
+    </>
+)}
+
+export default SignUp ;
